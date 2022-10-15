15da65f1b0f4968219c0b0e971c74eb126ff9bd7
1nsomnes <55066794+1nsomnes@users.noreply.github.com>
2022-10-14 22:14:43 -0700

    Working! :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate array_tool;
+use array_tool::vec::Intersect;
+
 fn main() {
 
 }
@@ -14,14 +17,14 @@ fn sudoku(puzzle: &mut [[u8; 9]; 9]) {
     }
 
     let mut boxes:[[[u8;9];3]; 3] = [[[0;9];3];3];
-
+    //boxes[y][x][box_index]
     for start_y in (0..9).step_by(3) {
         for start_x in (0..9).step_by(3) {
+            let mut index:usize = 0;
             for y in start_y..(start_y+3) {
                 for x in start_x..(start_x+3) {
-                    for index in 0..9 {
-                        boxes[start_y/3][start_x/3][index] = puzzle[y][x];
-                    }
+                    boxes[start_y/3][start_x/3][index] = puzzle[y][x];
+                    index += 1;
                 }
             }
         }
@@ -34,16 +37,68 @@ fn sudoku(puzzle: &mut [[u8; 9]; 9]) {
 
     let mut row_missing_values:Vec<Vec<u8>> = puzzle.iter().map(|row| find_missing_values(row)).collect();
     let mut columns_missing_values:Vec<Vec<u8>> = columns.iter().map(|column| find_missing_values(column)).collect();
+    let mut boxes_missing_values:[[Vec<u8>;3];3] = Default::default();
 
-    for row in row_missing_values {
-        //println!("Row:");
-        for m_v in row {
-            //println!("{}", m_v);
+    for y in 0..3 {
+        for x in 0..3 {
+            boxes_missing_values[y][x] = find_missing_values(&boxes[y][x]);
         }
     }
 
+    let remove_first_occurrence = |value: u8, list:&mut Vec<u8>| {
+        for i in 0..list.len() {
+            if list[i] == value {
+                list.remove(i);
+                break;
+            }
+        }
+    };
 
-    todo!()
+    let mut attempt_solve = |coords:(usize,usize)|  -> Option<u8> {
+        let row = &mut row_missing_values[coords.1];
+        let column = &mut columns_missing_values[coords.0];
+        let rbox = &mut boxes_missing_values[coords.1/3][coords.0/3];
+
+        let intersections = row.intersect(column.to_vec()).intersect(rbox.to_vec());
+
+        print!("\nIntersections at ({},{}):",coords.0,coords.1);
+        intersections.iter().for_each(|i| print!("{},", i));
+
+        if intersections.len() == 1 {
+
+            let value = intersections[0];
+            remove_first_occurrence(value, row);
+            remove_first_occurrence(value, column);
+            remove_first_occurrence(value, rbox);
+
+            println!("{}", value);
+
+            return Some(value);
+        }
+        None
+    };
+
+    fn is_solved(board:&[[u8; 9]; 9]) -> bool {
+        for y in 0..9 {
+            for x in 0..9 {
+                if board[y][x] == 0 { return false; }
+            }
+        }
+        true
+    }
+
+    while !is_solved(&puzzle) {
+        for y in 0..9 {
+            for x in 0..9 {
+                if &puzzle[y][x] == &0 {
+                    match attempt_solve((x, y)) {
+                        Some(value) => puzzle[y][x] = value,
+                        None => ()
+                    };
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -52,16 +107,17 @@ mod tests {
 
     #[test]
     fn puzzle_test_1() {
+        println!("\n\nTest 1:");
         let mut puzzle = [
-            [6, 0, 5, 7, 2, 0, 0, 3, 9],
-            [4, 0, 0, 0, 0, 5, 1, 0, 0],
-            [0, 2, 0, 1, 0, 0, 0, 0, 4],
-            [0, 9, 0, 0, 3, 0, 7, 0, 6],
-            [1, 0, 0, 8, 0, 9, 0, 0, 5],
-            [2, 0, 4, 0, 5, 0, 0, 8, 0],
-            [8, 0, 0, 0, 0, 3, 0, 2, 0],
-            [0, 0, 2, 9, 0, 0, 0, 0, 1],
-            [3, 5, 0, 0, 6, 7, 4, 0, 8],
+            [6, 0, 5, 7, 2, 4, 8, 3, 9],
+            [4, 8, 7, 3, 9, 5, 1, 6, 2],
+            [9, 2, 3, 1, 8, 6, 5, 7, 4],
+            [5, 9, 8, 4, 3, 2, 7, 1, 6],
+            [1, 3, 6, 8, 7, 9, 2, 4, 5],
+            [2, 7, 4, 6, 5, 1, 9, 8, 3],
+            [8, 4, 9, 5, 1, 3, 6, 2, 7],
+            [7, 6, 2, 9, 4, 8, 3, 5, 1],
+            [3, 5, 1, 2, 6, 7, 4, 9, 8],
         ];
         let solution = [
             [6, 1, 5, 7, 2, 4, 8, 3, 9],
@@ -81,6 +137,8 @@ mod tests {
 
     #[test]
     fn puzzle_test_2() {
+        println!("\n\nTest 2:");
+
         let mut puzzle = [
             [0, 0, 8, 0, 3, 0, 5, 4, 0],
             [3, 0, 0, 4, 0, 7, 9, 0, 0],
